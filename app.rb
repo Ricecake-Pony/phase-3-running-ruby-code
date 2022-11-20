0f75f64ab9dc8214d4ad51bdc9d750cfa246bd8b
Trey <trey.delcamp@gmail.com>
2022-11-20 16:55:56 -0600

    commit tree 260c20eb2c9ff708a88dcce67ce317c1347e72ee

---
+++ app.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# puts [1, 2, 3]
+print "Pass this test, please."
+p [1, 2, 3]
